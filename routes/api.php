a6b5d244d5dc70228cf4be17d6be9e5d68153f37
Egzona Sylejmani <egzonasylejmanni@gmail.com>
2024-09-29 16:17:12 +0200

    API Endpoints for Company Overview and Products

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\ProductController;
+
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+    Route::get('/company', [CompanyController::class, 'index']);
+    Route::post('/company', [CompanyController::class, 'store']);
+    Route::put('/company/{id}', [CompanyController::class, 'update']);
+    Route::delete('/company/{id}', [CompanyController::class, 'destroy']);
+
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+
