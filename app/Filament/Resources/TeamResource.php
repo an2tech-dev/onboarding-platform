8fde53089e91660fd61594d8a426af3a02d64a75
festinahuruglica <festinahuruglicaaa@gmail.com>
2024-10-03 17:14:33 +0200

    set up admin interface for floors and teams

---
+++ app/Filament/Resources/TeamResource.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TeamResource\Pages;
+use App\Models\Team;
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Resources\Resource;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Select;
+
+class TeamResource extends Resource
+{
+    protected static ?string $model = Team::class;
+
+    // protected static ?string $navigationIcon = 'heroicon-o-users';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make('floor_id')
+                    ->relationship('floor', 'name')
+                    ->required(),
+                TextInput::make('name')->required(),
+                TextInput::make('members_count')->numeric()->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('floor.name')->label('Floor'),
+                Tables\Columns\TextColumn::make('name')->label('Team Name'),
+                Tables\Columns\TextColumn::make('members_count')->label('Members Count'),
+            ])
+            ->filters([
+                // Add filters if necessary
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTeams::route('/'),
+            'create' => Pages\CreateTeam::route('/create'),
+            'edit' => Pages\EditTeam::route('/{record}/edit'),
+        ];
+    }
+}
