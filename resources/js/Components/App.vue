7379b5ffa4382ab511e64c7ea34fcf61f5add04e
Egzona Sylejmani <147817750+egzonasylejmani@users.noreply.github.com>
2024-11-04 14:20:35 +0100

    Correct import paths for Welcome.vue

--- resources/js/Components/App.vue
+++ resources/js/Components/App.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="app-container bg-[#F4F1FA] min-h-screen">
-    <NavBar />
     <router-view></router-view>
   </div>
 </template>
 
 <script setup>
-import NavBar from './NavBar.vue'; 
 </script>
 
 <style>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,21 +1,9 @@
-// src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../components/Home.vue';
-import Processes from '../components/Processes.vue';
-import Resources from '../components/Resources.vue';
-import Floor1 from '../components/Floor1.vue';
-import Floor2 from '../components/Floor2.vue';
-import Welcome from '../components/Welcome.vue';
+import Welcome from '../Components/Welcome.vue';
 
 const routes = [
-  { path: '/home', component: Home },
-  { path: '/processes', component: Processes },
-  { path: '/resources', component: Resources },
-  { path: '/teams/floor1', component: Floor1 },
-  { path: '/teams/floor2', component: Floor2 },
-  // { path: '/', redirect: '/home' },
-  { path: '/welcome', component: Welcome }, 
-  { path: '/', redirect: '/welcome' } 
+  { path: '/welcome', component: Welcome },
+  { path: '/', redirect: '/welcome' },
 ];
 
 const router = createRouter({
