77f8b07f6b992329c418cd4439be059aeceb11b8
Egzona Sylejmani <147817750+egzonasylejmani@users.noreply.github.com>
2024-11-12 14:15:08 +0100

    set up Vue with Vite in Laravel project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
-console.log("Minimal app.js loaded");
+import { createApp } from 'vue';
+import App from './components/App.vue';
+
+const app = createApp(App);
+app.mount('#app');
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <h1>Welcome to the Onboarding Platform</h1>
-    <p>This is the backend setup without any frontend framework.</p>
+    <div id="app"></div>
 </body>
 </html>
