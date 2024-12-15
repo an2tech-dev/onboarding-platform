0112be51b9d27b65adebde9daea363e5bbc0daf3
Andi Emini <142815247+andieminidev@users.noreply.github.com>
2024-12-15 23:14:02 +0100

    Add FloorSeeder with safe data insertion

--- database/seeders/FloorSeeder.php
+++ database/seeders/FloorSeeder.php
@@ -8,15 +8,49 @@ use Illuminate\Database\Seeder;
 
 class FloorSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        Company::all()->each(function ($company) {
-            Floor::factory(5)->create([
-                'company_id' => $company->id,
-            ]);
+        $predefinedFloors = [
+            [
+                'name' => 'Executive Suite',
+                'floor_number' => 20,
+            ],
+            [
+                'name' => 'Development Center',
+                'floor_number' => 15,
+            ],
+            [
+                'name' => 'Innovation Lab',
+                'floor_number' => 16,
+            ],
+            [
+                'name' => 'Conference Center',
+                'floor_number' => 2,
+            ],
+            [
+                'name' => 'Collaboration Space',
+                'floor_number' => 10,
+            ]
+        ];
+
+        Company::all()->each(function ($company) use ($predefinedFloors) {
+            foreach ($predefinedFloors as $floor) {
+                Floor::firstOrCreate(
+                    [
+                        'name' => $floor['name'],
+                        'company_id' => $company->id
+                    ],
+                    array_merge($floor, ['company_id' => $company->id])
+                );
+            }
+            
+            // Create additional random floors if needed
+            $floorCount = Floor::where('company_id', $company->id)->count();
+            if ($floorCount < 7) { // 5 predefined + 2 random
+                Floor::factory(7 - $floorCount)->create([
+                    'company_id' => $company->id,
+                ]);
+            }
         });
     }
 }
