3a62f44792967247285dd1359af821b19d8c692c
Andi Emini <142815247+andieminidev@users.noreply.github.com>
2024-12-24 00:23:15 +0100

    Created TeamSeeder with generated data

--- database/seeders/TeamSeeder.php
+++ database/seeders/TeamSeeder.php
@@ -14,7 +14,37 @@ class TeamSeeder extends Seeder
     public function run(): void
     {
         Company::all()->each(function ($company) {
-            Team::factory(5)->create([
+            $standardTeams = [
+                [
+                    'name' => 'Engineering Team',
+                    'description' => 'Core development and technical implementation team',
+                ],
+                [
+                    'name' => 'Product Team',
+                    'description' => 'Product strategy and management team',
+                ],
+                [
+                    'name' => 'Marketing Team',
+                    'description' => 'Marketing and brand management team',
+                ],
+                [
+                    'name' => 'HR Team',
+                    'description' => 'Human resources and personnel management',
+                ],
+                [
+                    'name' => 'Sales Team',
+                    'description' => 'Sales and business development',
+                ]
+            ];
+
+            foreach ($standardTeams as $team) {
+                Team::create([
+                    'company_id' => $company->id,
+                    ...$team
+                ]);
+            }
+
+            Team::factory(2)->create([
                 'company_id' => $company->id,
             ]);
         });
