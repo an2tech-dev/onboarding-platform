3ffbd5a4ad01e61c8a3a4290c6c4f80127b59cd9
Andi Emini <142815247+andieminidev@users.noreply.github.com>
2024-12-24 00:24:27 +0100

    Updated Database seeders with the new seeder implementations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,39 +2,24 @@
 
 namespace Database\Seeders;
 
-use App\Models\Team;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\User;
-use App\Models\Company;
-use App\Models\Stakeholder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         $this->seedRolesAndAdmins();
-        $admin = User::factory()->admin()->create();
-        $admin->assignRole('Administrator');
+        $this->seedCompaniesAndRelatedData();
+        $this->seedUsers();
     }
 
-    /**
-     * Seed roles and admin-related data.
-     */
     private function seedRolesAndAdmins(): void
     {
         $this->call([
             RolePermissionSeeder::class,
-            AdminSeeder::class,
         ]);
     }
 
-    /**
-     * Seed companies and related data.
-     */
     private function seedCompaniesAndRelatedData(): void
     {
         $this->call([
@@ -45,6 +30,14 @@ class DatabaseSeeder extends Seeder
             ProductSeeder::class,
             ResourceSeeder::class,
             StakeholderSeeder::class,
+            RoleInformationSeeder::class,
+        ]);
+    }
+
+    private function seedUsers(): void
+    {
+        $this->call([
+            UserSeeder::class,
         ]);
     }
 }
